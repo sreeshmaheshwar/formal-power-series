8e670af2f25525acd2352d9307c7873ad773c7e5
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2024-08-20 03:19:20 +0100

    Yosupo verification for multiplicative inverse

---
+++ verification/inverse.cpp
@@ -0,0 +1,30 @@
+// https://judge.yosupo.jp/problem/inv_of_formal_power_series
+
+#include "fps.hpp"
+#include <atcoder/convolution>
+#include <atcoder/modint>
+#include <iostream>
+
+using mint = atcoder::modint998244353;
+using PowerSeries = FormalPowerSeries<mint, [](const auto &a, const auto &b) {
+  return atcoder::convolution(a, b);
+}>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  std::cin >> n;
+
+  PowerSeries a(n);
+  for (int i = 0; i < n; ++i) {
+    int x;
+    std::cin >> x;
+    a[i] = x;
+  }
+
+  for (const auto &x : a.inverse(n)) {
+    std::cout << x.val() << ' ';
+  }
+}
