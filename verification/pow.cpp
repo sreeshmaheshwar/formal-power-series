193bf74f334fb334f3f2888753ea83c3033a0b6e
Sreesh Maheshwar <sreeshmaheshwar@gmail.com>
2024-08-21 05:35:44 +0100

    Add Yosupo verification of faster exponentiation

---
+++ verification/pow.cpp
@@ -0,0 +1,32 @@
+// https://judge.yosupo.jp/problem/pow_of_formal_power_series
+
+#include "fps.hpp"
+#include <atcoder/convolution>
+#include <atcoder/modint>
+#include <cstdint>
+#include <iostream>
+
+using mint = atcoder::modint998244353;
+using PowerSeries = FormalPowerSeries<mint, [](const auto &a, const auto &b) {
+  return atcoder::convolution(a, b);
+}>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  std::int64_t m;
+  std::cin >> n >> m;
+
+  PowerSeries a(n);
+  for (int i = 0; i < n; ++i) {
+    int x;
+    std::cin >> x;
+    a[i] = x;
+  }
+
+  for (const auto &x : a.pow(m, n)) {
+    std::cout << x.val() << ' ';
+  }
+}
